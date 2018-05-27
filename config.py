df9b029e5b6fad1f89f8bd05cb49d8a8225907ed
Bogdan Ostojic <bogdanostojic1993@gmail.com>
2018-05-27 14:08:33 +0200

    added .gitignore and fixed print error

--- config.py
+++
@@ -1,4 +0,0 @@
-username = "boty-boop"
-password = "alukobond93"
-client_id = "4klZH4KZ0rlujQ"
-client_secret = "gtwd2PxWANmBInX9T5IpgGNlUYU"
--- reddit_bot.py
+++ reddit_bot.py
@@ -4,8 +4,9 @@ import config
 import time
 
 def bot_login():
-	
-	r = praw.Reddit(username = config.username,
+
+	r = praw.Reddit(
+			username = config.username,
 			password = config.password,
 			client_id = config.client_id,
 			client_secret = config.client_secret,
@@ -15,16 +16,15 @@ def bot_login():
 
 def run_bot(r):
 	print("Obtaining 25 comments...")
-	
-	print "Sleeping for 10 seconds..."
+
+	print ("Sleeping for 10 seconds...")
 	try:
 		[comment.reply("I also love dogs! [Here](http://i.imgur.com/LLgRKeq.jpg) is an image of one!") for comment in r.subreddit('Test_Posts').comments(limit=25) if "dog" in comment.body]
 
 	except:
 		exception('Cannot comment!')
-			
-	time.sleep(10)
 
+	time.sleep(10)
 
 r = bot_login()
 while True:
